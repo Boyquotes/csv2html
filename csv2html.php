59ad25ee1e7ecfc1d865773249af7c1429ccc0d8
Boyquotes <nicolas@libre-shop.com>
2015-10-05 05:59:17 +0200

    Ajout de jquery et plugin autocomplete pour rechercher rapidement un candidat + rangement js/ css/

--- csv2html.php
+++ csv2html.php
@@ -2,28 +2,70 @@
 <head>
 	<meta charset="utf-8">
 	<title>Recrutement candidature</title>
-	<link rel="stylesheet" type="text/css" href="./csv2html.css"
+	
+	<link rel="stylesheet" href="./js/jquery-ui/jquery-ui.theme.css">
+	<link rel="stylesheet" type="text/css" href="./csv2html.css">
+
+	<script src="./js/jquery-2.1.4.min.js"></script>
+	<script src="./js/jquery-ui/jquery-ui.min.js"></script>
+	
+	<script>
+		jQuery(document).ready(function(){
+			$("#tags").autocomplete({
+			minLength: 2,
+			select: function(event, ui) {
+				if(ui.item){
+					//console.log(ui);
+					//console.log(ui.item);
+					//console.log(ui.item.value);
+				}
+				$('html, body').animate({
+					scrollTop: $("#"+ui.item.value).offset().top
+				}, 100);
+			}
+			});
+		});
+	</script>
 </head>
 
 <body>
 
+<div class="ui-widget">
+  <label for="tags">Candidat : </label>
+  <input id="tags">
+</div>
+
 <?php
 $f = fopen("candidatures.csv", "r");
+$fnotes = fopen("notes.csv", "r");
 $i = 0;
 $j = 0;
 $modulo = 0;
 $count = 0;
 $liste = '';
+$listeTags = '';
 
 while (($line = fgetcsv($f)) !== false) {
     $count = $count+1;
     $tab_candidat[] = $line;
     if($i != 0 ){
-        $liste .= "<li> ".$i." - <a href='#".$i."'>".$line[1]." ".$line[2]."</a></li>";
+        $liste .= "<li> ".$i." - <a href='#".$i."' id='#".$i."'>".$line[1]." ".$line[2]."</a></li>";
+        $listeTags .= '{ label:"'.$line[1].' '.$line[2].'", value:"'.$i.'" },';
 // - <a href='https://www.google.fr/search?q=".$line[1]."+".$line[2]."' target='_blank'> Googliser</a></li>";
     }
     $i++;
 }
+
+echo '<script>
+  $(function() {
+    var availableCandidats = [
+      '.$listeTags.'
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableCandidats
+    });
+  });
+  </script>';
 
 $i = 0;
 echo "Nombres de candidat.e.s: ";
@@ -34,7 +76,7 @@ echo "<ul class='list'>";
 echo "<div class='clear'></div>";
 echo "</ul>";
 
-echo "<ul>";
+
 foreach ($tab_candidat as $candidat) {
     $modulo = $i%2;
 
@@ -78,7 +120,7 @@ foreach ($tab_candidat as $candidat) {
 		}
     }
 	else{
-		$li = "<li class='box ".$alternance."' id='".$i."'>";
+		$li = "<div class='box ".$alternance."' id='".$i."'>";
 		echo $li;
 		$j = 0;
 		foreach ($candidat as $cell) {
@@ -100,13 +142,14 @@ foreach ($tab_candidat as $candidat) {
 			echo "</span>";
 			$j++;
 		}
-      echo "</li>";
+		echo "<div class='clear'></div>";
+		echo "</div>";
       }
-    echo "</ul>";   
     $i++;
 }
+
 fclose($f);
 ?>
-</ul>
+
 </body>
 </html>
